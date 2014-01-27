b35289091671077dfb79367fa26da789973bafad
mobssiie <mobssiie@tistory.com>
2014-01-27 23:32:36 +0900

    add 2-1 to SpringStudy

---
+++ 2-1/src/dao/TeamDao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.util.List;
+
+import model.Team;
+
+import org.springframework.dao.DataAccessException;
+
+public interface TeamDao {
+	List<Team> getTeamList() throws DataAccessException;
+}
