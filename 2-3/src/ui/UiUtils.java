774d07c216c9624b3d0e2f1591a430f699e1e63f
mobssiie <mobssiie@tistory.com>
2014-02-04 15:42:53 +0900

    add 2-3 to SpringStudy

---
+++ 2-3/src/ui/UiUtils.java
@@ -0,0 +1,28 @@
+package ui;
+
+import org.apache.commons.lang.StringUtils;
+
+public class UiUtils {
+	
+	public static boolean isSmallLength(String name, String fieldName, int length) {
+		if(name == null) {
+			return true;
+		}
+		
+		if(length < name.length()) {
+			System.out.printf("%s는 %s문자 이하로 입력해 주세요.",fieldName, length);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean isNumeric(String str, String fieldName) {
+		//숫자인지?
+		if(!StringUtils.isNumeric(str)) {
+			System.out.printf("%s는 숫자로 입력해 주세요.%n", fieldName);
+			return false;
+		}
+		return true;
+	}
+}
