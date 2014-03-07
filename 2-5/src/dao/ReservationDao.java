9e1d5dfc3a6bde9ef1c01df354ba91762a25539d
mobssiie <mobssiie@tistory.com>
2014-03-07 13:48:53 +0900

    add 2-5 to SpringStudy

---
+++ 2-5/src/dao/ReservationDao.java
@@ -0,0 +1,10 @@
+package dao;
+
+import model.Reservation;
+
+public interface ReservationDao {
+
+	void cancelReservation(Reservation reservation);
+	Reservation getReservation(Integer reservationId);
+
+}
